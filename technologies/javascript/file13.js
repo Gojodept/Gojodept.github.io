3562fc347890885e0594a3b5e7af687f0054a77d
gojodept <arindamkashyap625@gmail.com>
2025-06-24 00:37:38 +0530

    v0.7

---
+++ technologies/javascript/file13.js
@@ -0,0 +1,11 @@
+//spread operator
+let marks={
+    maths: 95,
+};
+marks={...marks,science:54};
+console.log(marks)
+
+
+let students =["Harsh","Ananta","Khushboo"]
+students=[...students,"Arindam"]
+console.log(students)
